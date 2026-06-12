dbaad1ee7e5cb2d1ff1b63d0a9f5e71898cf9e8e
Debdip-git <83181728+Dev664@users.noreply.github.com>
2026-06-12 13:48:13 +0530

    Create modified_app.py

---
+++ modified_app.py
@@ -0,0 +1,38 @@
+HTML = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CI/CD Demo</title>
+    <style>
+        body {
+            margin:0;
+            background:#0066cc;
+            color:white;
+            font-family:Arial;
+            text-align:center;
+            padding-top:200px;
+        }
+
+        .footer {
+            position:fixed;
+            bottom:10px;
+            right:20px;
+            font-size:12px;
+        }
+    </style>
+</head>
+<body>
+
+<h1>CI/CD Demo Application</h1>
+
+<h2>Version 1</h2>
+
+<p>Background Color: Blue</p>
+
+<div class="footer">
+Created by Debdip Ghosh
+</div>
+
+</body>
+</html>
+"""
